783f3d120cd5e56c5d8a5ffba453632ddadda57f
Alfredu <aleixsanchis@hotmail.com>
2015-04-12 17:40:01 +0200

    afegit metode afegirjugadors en controlador.java

--- Controlador.java
+++ Controlador.java
@@ -5,6 +5,7 @@
  */
 package entregable1;
 
+import java.util.Iterator;
 import java.util.TreeMap;
 
 /**
@@ -19,7 +20,7 @@ public class Controlador {
     
     //Constructor===//
     
-    public Controlador(){
+    public Controlador(InterficieUsuari iu){
         jugadors = new TreeMap();
         dau = new Dau();
         tauler = new Tauler();
@@ -27,6 +28,29 @@ public class Controlador {
     }
     
     //====Metodes===//
+    
+    public int afegeixJugador(String nomArg, String colorArg){
+        Iterator<String> it = jugadors.keySet().iterator();
+        int coincidencia = 0;
+   
+        while(it.hasNext() && coincidencia == 0){
+            String colorIterador = it.next();
+            
+            if(colorIterador.equalsIgnoreCase(colorArg)){                
+                coincidencia = -1;         
+            }
+        }
+        
+        switch (coincidencia){
+     
+                case    0 : 
+                    jugadors.put(colorArg, new Jugador(nomArg, colorArg, this.dau, this.tauler));
+                    return coincidencia;
+                default : return coincidencia;
+                    
+            }
+        
+    }
     
     
     
--- InterficieUsuari.java
+++ InterficieUsuari.java
@@ -11,4 +11,6 @@ package entregable1;
  */
 public class InterficieUsuari {
     
+    //Atributs=======//
+    
 }
